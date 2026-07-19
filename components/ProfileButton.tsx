e51c96a6a268599a4968efc75d7e7a176b29fdcf
haitham515 <haitamachour4@gmail.com>
2026-07-19 17:51:10 +0100

    Boutton pour redirection vers profile

--- components/ProfileButton.tsx
+++ components/ProfileButton.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import Link from 'next/link';
 import { IconUserCircle } from '@tabler/icons-react';
 
 export default function ProfileButton() {
   return (
     <Link
-      href="/api/profile"
+      href="/profile"
       className="flex flex-row items-center gap-2 bg-gray-800 text-white font-bold py-2 px-4 rounded hover:bg-gray-900 active:bg-black focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2 transition-colors w-fit"
     >
       <IconUserCircle size={18} />
